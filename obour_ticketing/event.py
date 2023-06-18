d973931cd7f4fb7bba9e00612d1faf9ee61394eb
tahirAnvil <tzaqout@anvilerp.com>
2023-06-18 11:59:59 +0300

    show all issues for user not in any group

--- obour_ticketing/event.py
+++ obour_ticketing/event.py
@@ -6,8 +6,8 @@ def get_permission_query_conditions(user):
         return ""
 
     ticketing_group = frappe.get_all("Ticketing User Table", filters={"user_email": user}, pluck="parent")
-    ticketing_group=",".join(["'{}'".format(item_group) for item_group in ticketing_group])
+    ticketing_group = ",".join(["'{}'".format(item_group) for item_group in ticketing_group])
     if ticketing_group:
         return """`tabIssue`.ticketing_group in ({})""".format(ticketing_group)
     else:
-        return "1=2"
+        return "1=1"
